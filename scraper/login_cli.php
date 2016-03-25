1da2de7fea1aa973d1bc2cbe6094caf2faccb983
datagutten <datagutten@datagutten.net>
2016-03-25 14:41:17 +0100

    Check if user is alredy logged in

--- scraper/login_cli.php
+++ scraper/login_cli.php
@@ -2,8 +2,14 @@
 require 'class_geocaching_com.php';
 $gc=new geocaching_com;
 
-$username=$gc->login($argv[1],$argv[2]);
-if($username===false)
-	echo "Login failed\n";
+$data=$gc->get('http://www.geocaching.com');
+if($username=$gc->is_logged_in($data))
+	echo "Already logged in as $username\n";
 else
-	echo "Logged in as $username\n";
+{
+	$username=$gc->login($argv[1],$argv[2]);
+	if($username===false)
+		echo "Login failed\n";
+	else
+		echo "Logged in as $username\n";
+}
